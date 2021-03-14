65e4ff674dc6d0b3123b6f0c2e9a70d7049d6496
eysanarli <you@example.com>
2021-03-15 00:19:33 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CarComponent } from './components/car/car.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BrandComponent,
+    ColorComponent,
+    NaviComponent,
+    CustomerComponent,
+    RentalComponent,
+    CarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
